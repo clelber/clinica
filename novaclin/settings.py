097267d14bc40927b129873ed3c4840ff93ec4cc
clelber <simoes.rar@gmail.com>
2021-01-17 10:59:14 -0300

    staticfiles

--- novaclin/settings.py
+++ novaclin/settings.py
@@ -111,8 +111,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 LOGIN_REDIRECT_URL = 'home'
